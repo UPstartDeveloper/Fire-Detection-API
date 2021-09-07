45448815de613d67a5a25ba791ec4d92ca0aafe7
= <zainr7989@gmail.com>
2021-09-07 11:51:38 -0400

    Reformatting.

--- app/main_test.py
+++ app/main_test.py
@@ -7,22 +7,20 @@ from app.main import app
 
 client = TestClient(app)
 
+
 class ClassifierAPITest(unittest.TestCase):
 
     TEST_COLOR_IMG_URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
 
     def test_make_prediction_normal(self):
-        '''Client submits a RGB image and gets a valid response.'''
+        """Client submits a RGB image and gets a valid response."""
         # A: get image
         image_path = tf.keras.utils.get_file(
-            origin=self.TEST_COLOR_IMG_URL, 
-            cache_dir=".", cache_subdir="./test-assets"
+            origin=self.TEST_COLOR_IMG_URL, cache_dir=".", cache_subdir="./test-assets"
         )
-        with open(image_path, 'rb') as f:
+        with open(image_path, "rb") as f:
             # B: format a mock request
             files = {"file": (f.name, f, "multipart/form-data")}
             # C: make a request, and verify a valid prediction is returned
-            response = client.post(
-                '/detect-fire', files=files
-            )
+            response = client.post("/detect-fire", files=files)
             assert response.status_code == 307
