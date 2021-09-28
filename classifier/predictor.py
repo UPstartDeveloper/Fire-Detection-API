fc987e081482b56036e4ae0bb2861babf01163c2
= <zainr7989@gmail.com>
2021-09-28 17:14:27 -0400

    Reuse smaller CNN.

--- classifier/predictor.py
+++ classifier/predictor.py
@@ -29,11 +29,10 @@ class ImagePredictor:
     def predict_from_array(self, arr) -> Dict[str, float]:
         """Returns a prediction value the sample belongs to each class."""
         # in this model, 'Normal Images' is the positive class (labeled by 1)
-        pred_arr = self.model.predict(arr[np.newaxis, ...]).ravel()
-        pred = tf.keras.activations.sigmoid(pred_arr).numpy().tolist()
+        pred = self.model.predict(arr[np.newaxis, ...]).ravel().tolist()
         # so we convert the probability to predict for 'Fire_Images'
         return {
-            class_label: (1 - prob) for class_label, prob in zip(self.targets, pred)
+            class_label: float(1.0 - prob) for class_label, prob in zip(self.targets, pred)
         }
 
     def predict_from_file(self, file_object):
